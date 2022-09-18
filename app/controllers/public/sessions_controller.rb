1c674ea2ded69ee6cb71fa386a58984289e52a67
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-18 02:32:13 +0000

    [Addアソシエーション]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,17 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def after_sign_up_path_for
+    my_page_path
+  end
+
+  def after_sign_in_path_for
+    root_path
+  end
+
+  def after_sign_out_path_for
+    root_path
+  end
+
 end
