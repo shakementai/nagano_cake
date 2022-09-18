6f72716da12758ba52d8789a41e2bab7500dc2c6
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-09-18 05:39:54 +0000

    [Add]index

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,13 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genre
-  
+
   has_one_attached :image
-  
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no-image.jpg')
