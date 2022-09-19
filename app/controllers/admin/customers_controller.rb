a14031e3123f456188aac9ec004b6727bf6b358c
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-19 09:08:28 +0000

    [Add管理側会員ページ]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,26 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_kana_name, :first_kana_name, :post_code, :address, :phone_number, :is_deleted)
+  end
+  
 end
