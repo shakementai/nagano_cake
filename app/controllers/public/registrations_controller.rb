09385f04c9dcb7be1eede7f5ee58f5ca322d3bcb
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-19 13:59:07 +0000

    フォーム編集

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, if: :devise_controller
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   def after_sign_up_path_for(current_customer)
