40ba8f28225d79f017b61cbdefc2110bed4ddb41
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-19 07:42:03 +0000

    機能完成

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -8,14 +8,15 @@ class Public::DeliveriesController < ApplicationController
   end
 
   def create
-    delivery = Delivery.new(delivery_params)
-    delivery.save
+    @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
+    @delivery.save
     redirect_to request.referer
   end
 
   private
 
   def delivery_params
-    params.require(:delivery).permit(:post_code, :address, :name, customer_id: current_customer.id)
+    params.require(:delivery).permit(:post_code, :address, :name)
   end
 end
