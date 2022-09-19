de48b2dd7e55ef22521d7b0b7f409c7827167ef8
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-09-19 11:23:07 +0000

    [Add]商品詳細ページ

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,22 @@
 class Public::CartItemsController < ApplicationController
+
   def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 end
