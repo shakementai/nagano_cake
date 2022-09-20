55a3a5b01b7bf8c4ad4ad75d1aa414a7dba28f80
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-20 02:37:52 +0000

    機能完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,8 +12,9 @@ class Public::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to my_page_path(@customer.id)
   end
-  
+
   private
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_kana_name, :first_kana_name, :post_code, :address, :phone_number, :email)
   end
