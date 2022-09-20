f7347317b307ab32f110dde2424645d5807e37f8
shoaws <108819031+shoaws@users.noreply.github.com>
2022-09-20 01:16:38 +0000

    [Addkaminari]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,6 +1,8 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
-    @customers = Customer.all
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
@@ -10,17 +12,17 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
-  
+
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
     redirect_to admin_customer_path(@customer)
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_kana_name, :first_kana_name, :post_code, :address, :phone_number, :is_deleted)
   end
-  
+
 end
