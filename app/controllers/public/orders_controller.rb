9c7c3c4a4e1a65b62c1977c099fcf4d7cb7c8903
Satomi31 <108987382+Satomi31@users.noreply.github.com>
2022-09-21 05:49:28 +0000

    [Add]支払方法

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,10 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def complete
-    
+
   end
 
   def index
@@ -11,4 +12,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  # private
+
+  # def order_params
+  #   params.require(:order).permit(:customer_id, :post_code, :address, :name, :payment_method )
+  # end
+
 end
