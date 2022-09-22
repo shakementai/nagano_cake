e82aa0b2c966470b4b3f5201820bdcc2d63bcd01
Tsubakurouuu <yzpolice2896@icloud.com>
2022-09-22 14:23:45 +0000

    会員詳細画面でパスワードのバリデーションがかかる

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,14 +1,11 @@
 class Delivery < ApplicationRecord
   belongs_to :customer
-<<<<<<< HEAD
-  validates :post_code, presence: true
-  validates :address, presence: true
-  validates :name, presence: true
-=======
 
   def address_display
   '〒' + post_code + ' ' + address + '　' + name
   end
 
->>>>>>> origin/develop
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 end
