4c531903af509b7ae9c6ecaa3b8f7b4407b52982
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-22 09:45:42 +0000

    [Fix]showページのview

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,7 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
   def show
     @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
   
   def update
