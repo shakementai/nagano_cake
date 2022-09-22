85f12312ae09f2edd53c14036096f77d16091e15
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-22 08:19:30 +0000

    [Add]コントローラーとviewのセレクトボックス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,10 +2,22 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.save(order_params)
+    redirect_to request.referer
+  end
 
   def index
     @customer = Customer.find(params[:id])
     @orders = Order.page(params[:page]).per(10)
   end
 
+  private
+  
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+  
 end
