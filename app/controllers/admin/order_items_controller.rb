f69926e06d9ad9cc692dee3724d086ddc745f684
hkreds <109258421+hkreds@users.noreply.github.com>
2022-09-23 08:40:58 +0000

    [Fix]showページの金額表示

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,14 @@
 class Admin::OrderItemsController < ApplicationController
+  def update
+    @order_item = OrderItem.find(params[:id])
+    if @order_item.update(order_item_params)
+      redirect_to request referer
+    end
+  end
+  
+  private
+  
+  def order_item_params
+    params.require(:order_item).permit(:production_status)
+  end
 end
